5c609399596a71e47ab5cc6f10f35999cbe4c848
SAAD LARHRIB <saad.larhrib06@gmail.com>
2026-09-09 21:53:34 +0100

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        map<char, int> dicts;
+        map<char, int> dictt;
+
+        if(s.length() == t.length()){
+            for(int i = 0; i < s.length(); i++){
+                dicts[s[i]]++;
+            }
+
+            for(int i = 0; i < t.length(); i++){
+                dictt[t[i]]++;
+            }
+
+            if(dicts == dictt){
+                return true;
+            }
+        }
+        return false;
+    }
+};
